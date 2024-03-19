0336c1bfa51a9689fde9896fd088b3324f10e614
naimuddin94 <developernaimuddin@gmail.com>
2024-03-19 18:01:23 +0600

    redux store setup

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,16 @@ import "./css/style.css";
 import "./css/satoshi.css";
 import router from "./Routes/Route";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Provider } from "react-redux";
+import store from "./redux/store/store";
 
 // Create a client
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />
-  </QueryClientProvider>
+  <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </Provider>
 );
